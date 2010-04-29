37bdf1f48de92d32441ef34ae97553ecac2b9d95
Mat Schaffer <mat.schaffer@gmail.com>
2010-04-29 00:11:41 -0400

    Added support for clearing and swapping prefixes. Also made calls a hash hoping that I can include things other than arguments shortly.

--- spec/unit/spec.recorderMock.js
+++ spec/unit/spec.recorderMock.js
@@ -2,6 +2,7 @@ JSpec.describe('Recorder mock', function () {
   var recorder;
 
   before_each(function () {
+    recorderMock.prefix = "__";
     recorder = recorderMock("foo", "bar");
   });
 
@@ -17,12 +18,44 @@ JSpec.describe('Recorder mock', function () {
   it("should record calls to member functions", function () {
     recorder.foo("some", "arguments");
     expect(recorder.foo.__calls.length).to(eql, 1);
-    expect(recorder.foo.__calls[0]).to(eql, ["some", "arguments"]);
+    expect(recorder.foo.__calls[0].arguments).to(eql, ["some", "arguments"]);
   });
 
   it("should record calls to as a function", function () {
     recorder("some", "arguments");
     expect(recorder.__calls.length).to(eql, 1);
-    expect(recorder.__calls[0]).to(eql, ["some", "arguments"]);
+    expect(recorder.__calls[0].arguments).to(eql, ["some", "arguments"]);
+  });
+
+  it("should clear calls to specific functions", function () {
+    recorder.foo();
+    recorder.bar();
+    expect(recorder.foo.__calls).not_to(be_empty);
+    recorder.foo.__clear();
+    expect(recorder.foo.__calls).to(be_empty);
+    expect(recorder.bar.__calls).not_to(be_empty);
+  });
+
+  it("should clear calls to all member functions", function () {
+    recorder.foo();
+    recorder.bar();
+    expect(recorder.foo.__calls).not_to(be_empty);
+    expect(recorder.bar.__calls).not_to(be_empty);
+    recorder.__clearAll();
+    expect(recorder.foo.__calls).to(be_empty);
+    expect(recorder.bar.__calls).to(be_empty);
+  });
+
+  it("should mimic signature of object if given", function () {
+    recorder = recorderMock({baz: 0, qux: 1});
+    recorder.qux();
+    expect(recorder.qux.__calls).not_to(be_empty);
+  });
+
+  it("can use other special method prefixes if needed", function () {
+    recorderMock.prefix = "";
+    recorder = recorderMock("foo");
+    recorder.foo();
+    expect(recorder.foo.calls).not_to(be_empty);
   });
 });
